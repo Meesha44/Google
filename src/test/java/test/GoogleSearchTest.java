8a745ac227fcd06ce0e1e24eb198bf0725c83784
Meesha <meesha.kmr@gmail.com>
2024-01-15 14:46:18 +0530

    Second Commit

--- src/test/java/test/GoogleSearchTest.java
+++ src/test/java/test/GoogleSearchTest.java
@@ -27,6 +27,7 @@ public class GoogleSearchTest extends Base{
 	{
 	Boolean result;
 	try {
+		System.out.print("first test case");
 		result = GoogleSearch.SearchResult("Selenium");
 		Assert.assertTrue(result);
 	} catch (InterruptedException e) {
