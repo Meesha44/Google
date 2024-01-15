5c34ba4d0487eafb642637359e2983a8f439eafd
Meesha <meesha.kmr@gmail.com>
2024-01-15 19:34:08 +0530

    Commiting 2nd test case

---
+++ src/test/java/test/GoogleSearchTest2.java
@@ -0,0 +1,46 @@
+package test;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import base.Base;
+import pages.GoogleSearchPage;
+
+public class GoogleSearchTest2 extends Base{
+	GoogleSearchPage GoogleSearch;
+	public GoogleSearchTest2()
+	{
+		super();
+	}
+	
+	@BeforeMethod
+	public void SetUp()
+	{
+		Initialization();
+		GoogleSearch= new GoogleSearchPage();
+	}
+	
+	@Test
+	public void SearchResultTest()
+	{
+	Boolean result;
+	try {
+		System.out.print("first test case");
+		result = GoogleSearch.SearchResult("Selenium");
+		Assert.assertTrue(result);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	}
+	
+	@AfterMethod
+	public void TearDown()
+	{
+		driver.quit();
+	}
+	
+}
